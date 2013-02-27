300a91c03c81feb2010a29be0b44d58a20515695
Mike Izbicki <mike@izbicki.me>
2013-02-26 23:16:43 -0800

    works, needs cleaning and features

--- src/Data/NTuple.hs
+++ src/Data/NTuple.hs
@@ -17,8 +17,9 @@
 {-# LANGUAGE TypeFamilies #-}
 
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE OverlappingInstances #-}
 
-module Data.NNTuple'
+module Data.NTuple'
     where
 
 import Control.Lens
@@ -35,52 +36,184 @@ data Color = Red | Blue | Green | Purple
 -------------------------------------------------------------------------------
 -- data types
 
-data NTuple (xs :: [*]) = forall n. NTuple (NTuple' n xs)
+data UnsafeBox = UnsafeBox
+    deriving (Read,Show)
 
-data NTuple' (n::Nat) (xs::[*]) = NTuple' 
+data NTuple' (xs::[*]) = NTuple' 
     { len :: Int
-    , getvec :: V.Vector Int
+    , getvec :: V.Vector UnsafeBox
     }
 
+data a:::b = b:::a
+    deriving (Read, Show, Eq, Ord)
 
--- class CountArgs t where
---     countArgs :: Int -> t
---  
--- instance CountArgs Int where
---     countArgs args = args
---  
--- instance (CountArgs r) => CountArgs (a -> r) where
---     countArgs count = \a -> countArgs (count + 1)
---  
--- -- count :: (CountArgs t) => t
--- count args = NTuple' 
+class Len t where
+    tlen :: t -> Int
+    
+instance (Len b) => Len (a ::: b) where
+    tlen (b ::: a) = 1 + tlen b
+
+instance Len b where
+    tlen b = 1
+
+class TupleWriter t where
+    tupwrite :: VM.MVector s UnsafeBox -> Int -> t -> ST s ()
+
+instance (TupleWriter b) => TupleWriter (a:::b) where
+    tupwrite v i (b:::a) = VM.write v i (unsafeCoerce a) >> tupwrite v (i-1) b
+
+instance TupleWriter b where
+    tupwrite v i b = VM.write v i (unsafeCoerce b)
+
+newtype Tuple xs = Tuple (V.Vector UnsafeBox)
+
+tup :: (TupleWriter a, Len a) => a -> Tuple a
+tup xs = Tuple $ V.create $ do
+    v <- VM.new n
+    tupwrite v (n-1) xs
+--     go v xs (n-1)
+    return v
+    where
+        n = tlen xs
+        
 --     where
---         n = countArgs 0 args
+--         go v (x:[]) 0 = VM.write v 0 x
+--         go v (x:xs) i = VM.write v i x >> go v xs (i-1)
+
+-- tup :: a:::b -> 
+
+newtype HList (xs ::[*]) = HList [UnsafeBox]
+    deriving (Read,Show)
+
+hnil :: HList '[]
+hnil = HList []
+
+hcons :: a -> HList xs -> HList (a ': xs)
+hcons a (HList xs) = HList ((unsafeCoerce a):xs)
+
+hlist2tuple :: HList xs -> NTuple' xs
+hlist2tuple (HList xs) = NTuple'
+    { len = n
+    , getvec = vec
+    }
+    where 
+        n = length xs
+        vec = V.create $ do
+            v <- VM.new n
+            go v xs (n-1)
+            return v
+            
+        go v (x:[]) 0 = VM.write v 0 x
+        go v (x:xs) i = VM.write v i x >> go v xs (i-1)
+        
+
+class MkList t1 t2 | t2 -> t1 where
+    mklist :: t1 -> t2
+ 
+-- instance MkList (HList xs) (HList xs) where
+--     mklist args = args
+
+instance MkList (HList '[]) (HList '[]) where
+    mklist args = HList []
+
+instance (MkList (HList (a ': xs)) r) => MkList (HList xs) (a -> r) where
+    mklist list = \a -> mklist (a `hcons` list)
+
+-- test = hlist2tuple $ mklist (HList []) "poop" (89::Int) 'h'
+
+-- terminate :: HList xs -> HList
+
+-- class MkList t where
+--     mklist :: [String] -> t
+--  
+-- instance (Show a) => MkList (IO a) where
+--     mklist args = do
+--         print args
+--         return undefined
+--  
+-- instance (MkList r, Show a) => MkList (a -> r) where
+--     mklist xs = \a -> mklist ((show a) : xs)
+-- 
+-- tup = mklist []
+
+-- class PrintAllType t where
+--     process :: [String] -> t
+--  
+-- instance PrintAllType [String] where
+--     process args = args
+--  
+-- instance (Show a, PrintAllType r) => PrintAllType (a -> r) where
+--     process args = \a -> process (args ++ [show a])
+--  
+-- printAll :: (PrintAllType t) => t
+-- printAll = process ([] :: [String])
+
+-- mkl = mklist []
+
+-- class MkList t where
+--     mklist :: HList xs -> t
+--  
+-- instance MkList (HList '[]) where
+--     mklist args = hnil
+--  
+-- instance (MkList r) => MkList (a -> r) where
+--     mklist count = \a -> mklist (a `hcons` count)
+-- 
+-- mkl = mklist hnil
 
 -------------------------------------------------------------------------------
 -- showing
 
 data ShowBox a = ShowBox Int a
 
-instance Show (ShowBox (NTuple' n '[])) where
+-- instance Show (ShowBox (Tuple a) where
+--     show a = ""
+    
+-- instance (Show (ShowBox (Tuple' xs))) => Show (NTuple' xs) where
+--     show tup = "(tup " ++ (show $ ShowBox ((len tup)-1) tup) ++ ")"
+--         where
+--             n = V.length (getvec tup)
+-- --             n = fromIntegral $ fromSing (sing :: Sing n)    
+
+instance (Show (ShowBox (Tuple a))) => Show (Tuple a) where
+    show a = "(tup $ "++(show $ ShowBox (len-1) a)++")"
+        where
+            len = let (Tuple vec) = a in V.length vec
+
+instance 
+    ( Show a
+    , Show (ShowBox (Tuple b))
+    ) => Show (ShowBox (Tuple (a:::b))) where
+    show (ShowBox i (Tuple vec)) = 
+        show (ShowBox (i-1) (Tuple vec :: Tuple b))++":::"++show (unsafeCoerce (vec V.! i) :: a)
+    
+instance 
+    ( Show b
+    ) => Show (ShowBox (Tuple b)) where
+    show (ShowBox i (Tuple vec)) = show (unsafeCoerce (vec V.! i) :: b)
+        
+
+instance Show (ShowBox (NTuple' '[])) where
     show _ = ""
     
-instance (Show (ShowBox (NTuple' n xs)), SingI n) => Show (NTuple' n xs) where
+instance (Show (ShowBox (NTuple' xs))) => Show (NTuple' xs) where
     show tup = "(tup " ++ (show $ ShowBox ((len tup)-1) tup) ++ ")"
         where
-            n = fromIntegral $ fromSing (sing :: Sing n)    
+            n = V.length (getvec tup)
+--             n = fromIntegral $ fromSing (sing :: Sing n)    
 
 instance 
     ( Show x
-    , Show (ShowBox (NTuple' n xs))
-    , SingI n
-    ) => Show (ShowBox (NTuple' n (x ': xs))) where
+    , Show (ShowBox (NTuple' xs))
+--     , SingI n
+    ) => Show (ShowBox (NTuple' (x ': xs))) where
     show (ShowBox i tup) = show (unsafeCoerce (getvec tup V.! i) :: x)++
         if i>0
-           then " "++show (ShowBox (i-1) $ (unsafeCoerce tup::(NTuple' n xs)))
+           then " "++show (ShowBox (i-1) $ (unsafeCoerce tup::(NTuple' xs)))
            else ""
         where
-            n = fromIntegral $ fromSing (sing :: Sing n)
+            n = V.length (getvec tup)
+--             n = fromIntegral $ fromSing (sing :: Sing n)
 
 -------------------------------------------------------------------------------
 -- modification
@@ -88,29 +221,38 @@ instance
 class EmptyTup tup where
     emptytup :: tup
 
-instance (SingI n) => EmptyTup (NTuple' n xs) where
+instance EmptyTup (NTuple' xs) where
     emptytup = NTuple'
         { len = 0
-        , getvec = V.replicate n 0
+        , getvec = V.replicate n UnsafeBox
         }
         where
-            n = fromIntegral $ fromSing (sing :: Sing n)
+            n = 2
+--           n = fromIntegral $ fromSing (sing :: Sing n)
 
 class PushBack tup a tup' | tup a -> tup' where
     pushback :: tup -> a -> tup'
     
--- instance (Length xs <= n) => PushBack (NTuple' n xs) a (NTuple' n (a ': xs)) where
-instance PushBack (NTuple' n xs) a (NTuple' n (a ': xs)) where
-    pushback tup a = NTuple'
-        { len = (len tup)+1
-        , getvec = runST $ do
-            v <- V.unsafeThaw $ (getvec tup)
-            VM.write v (len tup) (unsafeCoerce a)
-            V.unsafeFreeze v
-        }
+instance PushBack (NTuple' xs) a (NTuple' (a ': xs)) where
+    pushback tup a = if n<V.length (getvec tup)
+        then NTuple'
+            { len = (len tup)+1
+            , getvec = runST $ do
+                v <- V.unsafeThaw $ (getvec tup)
+                VM.write v (len tup) (unsafeCoerce a)
+                V.unsafeFreeze v
+            }
+        else NTuple'
+            { len = (len tup)+1
+            , getvec = V.generate ((len tup)*2) $ \i -> if i<(len tup)
+                then (getvec tup) V.! i
+                else unsafeCoerce a
+            }
+        where
+            n = len tup
 
 class TupIndexable index where
-    index :: NTuple' n xs -> index -> xs :! (ToNat1 (ExtractIndex index))
+    index :: NTuple' xs -> index -> xs :! (ToNat1 (ExtractIndex index))
     
 instance (SingI i) => TupIndexable (Index i) where
     index tup Index = unsafeCoerce (getvec tup V.! i)
@@ -128,7 +270,17 @@ type family Length (xs::[*]) :: Nat
 type instance Length '[] = 0
 type instance Length (a ': xs) = 1 + (Length xs)
 
-type family (:!) (xs::[*]) (i::Nat1) :: *
+type family MoveR (xs::[*]) (ys::[*]) :: [*]
+type instance MoveR '[] ys = ys
+type instance MoveR (x ': xs) ys = MoveR xs (x ': ys)
+
+type family Reverse (xs::[*]) :: [*]
+type instance Reverse xs = MoveR xs '[]
+
+type family (xs::[*]) ++ (ys::[*]) :: [*]
+type instance xs ++ ys = MoveR (Reverse xs) ys
+
+type family (:!) (xs::[a]) (i::Nat1) :: a
 type instance (:!) (x ': xs) Zero = x
 type instance (:!) (x ': xs) (Succ i) = xs :! i
 
@@ -164,10 +316,13 @@ type instance ToNat1 20 = Succ (ToNat1 19)
 -------------------------------------------------------------------------------
 -- tests
 
-tup1 = NTuple' 2 (V.replicate 2 45) :: NTuple' 1 '[Int]
-tup2 = NTuple' 2 (V.replicate 2 45) :: NTuple' 2 '[Int,Int]
-tup4 = NTuple' 4 (V.fromList [45,(unsafeCoerce 'a'),(unsafeCoerce "test"),(unsafeCoerce (10::Double))]) :: NTuple' 4 '[Int,Char,String,Double]
+-- tup1 = NTuple' 2 (V.replicate 2 45) :: NTuple' 1 '[Int]
+-- tup2 = NTuple' 2 (V.replicate 2 45) :: NTuple' 2 '[Int,Int]
+-- tup4 = NTuple' 4 (V.fromList [45,(unsafeCoerce 'a'),(unsafeCoerce "test"),(unsafeCoerce (10::Double))]) :: NTuple' 4 '[Int,Char,String,Double]
 
-e3 = emptytup :: (NTuple' 3 '[])
-e8 = emptytup :: (NTuple' 8 '[])
+e3 = emptytup :: (NTuple' '[])
+e8 = emptytup :: (NTuple' '[])
 
+tup3 = pushback (pushback (pushback e3 "test") 89) 'h'
+
+hl = hcons 34 $ hcons 'g' $ hcons "test" hnil
